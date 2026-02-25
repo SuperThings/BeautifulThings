ec935f2bc64ceeb59ec5b2c7713547aec46d881d
Rico <outstandexcellence@gmail.com>
2026-02-25 09:58:23 +0000

    counter.c

---
+++ C/counter/counter.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int counter = 0;
+    int n = 100;
+            while (counter < n)
+            {
+                counter +=1;
+                printf("The count is now %i.\n", counter);
+            if (counter == 100)
+            {
+                printf("You have reached the Max level 100.\n");
+            }
+            }
+
+
+}
